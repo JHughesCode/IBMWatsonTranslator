df1dc6ad9d6f1230177ce5b9c9d7eca1de825207
Jade Hughes <jadehughes295@hotmai.com>
2022-10-13 15:36:47 +0100

    Rewrote server.py

--- server.py
+++ server.py
@@ -3,33 +3,19 @@ import translator
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
+@app.route("/engtofrench/<word>")
+def goFrench(word):
+    return translator.e2f(word)
 
-@app.route("/EnglishtoFrench")
-def EnglishtoFrench():
-    return
 
-@app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    french_text = translator.translator.e2f(textToTranslate)
-    return french_text
-
-@app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    english_text = translator.translator.f2e(textToTranslate)
-    return english_text
+@app.route("/frenchtoeng/<word>")
+def goEnglish(word):
+    return translator.f2e(word)
 
 @app.route("/")
 def renderIndexPage():
     return render_template('index.html')
 
-@app.route("/about")
-def view_about_page():
-    return render_template("about.html", title="About project")
 
 if __name__ == "__main__":
     app.run(debug=True)
